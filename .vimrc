a4edf9a1e471b41521fa7fcf37226f1e74243bd7
Michael Scheiwiller <mi.lappert@gmail.com>
2024-05-13 23:23:15 +0200

    update

---
+++ .vimrc
@@ -0,0 +1,42 @@
+" Specify a directory for plugins
+call plug#begin('~/.vim/plugged')
+
+" Add plugins here
+Plug 'morhetz/gruvbox'          " Gruvbox theme
+Plug 'tpope/vim-sensible'       " Sensible defaults for Vim
+Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }  " Fuzzy finder
+Plug 'junegunn/fzf.vim'         " FZF integration for Vim
+Plug 'scrooloose/nerdtree'      " File explorer
+Plug 'airblade/vim-gitgutter'   " Git integration
+
+" Initialize plugin system
+call plug#end()
+
+" Set Gruvbox as the colorscheme
+syntax enable
+set background=dark
+colorscheme gruvbox
+
+" General settings
+set number                  " Show line numbers
+set relativenumber          " Show relative line numbers
+set expandtab               " Use spaces instead of tabs
+set tabstop=4               " Number of spaces tabs count for
+set shiftwidth=4            " Number of spaces to use for each step of (auto)indent
+set softtabstop=4           " Number of spaces that a <Tab> counts for while performing editing operations
+set autoindent              " Copy indent from current line when starting a new line
+set smartindent             " Do smart autoindenting when starting a new line
+set hlsearch                " Highlight search matches
+set incsearch               " Show search matches as you type
+set ignorecase              " Ignore case when searching
+set smartcase               " Override ignorecase if search contains capitals
+set clipboard=unnamedplus   " Use system clipboard
+set cursorline              " Highlight the current line
+set termguicolors           " Enable 24-bit RGB colors
+
+" NERDTree settings
+map <C-n> :NERDTreeToggle<CR>    " Toggle NERDTree with Ctrl+n
+
+" FZF settings
+map <C-p> :Files<CR>             " Open FZF file finder with Ctrl+p
+
--- setup.sh
+++ setup.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-# Install Oh My Zsh
+# Install Oh My Zsh if not installed
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-# Install plugins
-ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+# Define ZSH_CUSTOM if not already set
+ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
 
-# zsh-autosuggestions
+# Install Powerlevel10k theme
+if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+fi
+
+# Install zsh-autosuggestions
 if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
   git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 fi
 
-# zsh-syntax-highlighting
+# Install zsh-syntax-highlighting
 if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 fi
 
-# zsh-completions
+# Install zsh-completions
 if [ ! -d "$ZSH_CUSTOM/plugins/zsh-completions" ]; then
   git clone https://github.com/zsh-users/zsh-completions $ZSH_CUSTOM/plugins/zsh-completions
 fi
@@ -28,19 +33,29 @@ if [ ! -d "$ZSH_CUSTOM/plugins/zsh-history-substring-search" ]; then
   git clone https://github.com/zsh-users/zsh-history-substring-search $ZSH_CUSTOM/plugins/zsh-history-substring-search
 fi
 
+# Install zsh-nvm
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-nvm" ]; then
+  git clone https://github.com/lukechilds/zsh-nvm $ZSH_CUSTOM/plugins/zsh-nvm
+fi
+
 # Install zsh-you-should-use
-if [ ! -d "$ZSH_CUSTOM/plugins/you-should-use" ]; then
-  git clone https://github.com/MichaelAquilina/zsh-you-should-use $ZSH_CUSTOM/plugins/you-should-use
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-you-should-use" ]; then
+  git clone https://github.com/MichaelAquilina/zsh-you-should-use $ZSH_CUSTOM/plugins/zsh-you-should-use
 fi
 
 # Install zsh-direnv
-if [ ! -d "$ZSH_CUSTOM/plugins/direnv" ]; then
-  git clone https://github.com/direnv/direnv $ZSH_CUSTOM/plugins/direnv
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-direnv" ]; then
+  git clone https://github.com/direnv/direnv $ZSH_CUSTOM/plugins/zsh-direnv
 fi
 
 # Install zsh-interactive-cd
-if [ ! -d "$ZSH_CUSTOM/plugins/interactive-cd" ]; then
-  git clone https://github.com/changyuheng/zsh-interactive-cd $ZSH_CUSTOM/plugins/interactive-cd
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-interactive-cd" ]; then
+  git clone https://github.com/changyuheng/zsh-interactive-cd $ZSH_CUSTOM/plugins/zsh-interactive-cd
+fi
+
+# Install zsh-vi-mode
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-vi-mode" ]; then
+  git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
 fi
 
 # Install zsh-z
@@ -48,13 +63,28 @@ if [ ! -d "$ZSH_CUSTOM/plugins/zsh-z" ]; then
   git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
 fi
 
-# Powerlevel10k theme
-if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-fi
-
-# Symlink dotfiles to home directory
+# Create symlinks for dotfiles
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.fzf.zsh ~/.fzf.zsh
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/tmux-dracula ~/tmux-dracula
 
-echo "Dotfiles setup completed, don't forget to restart the terminal."
+# For Neovim
+mkdir -p ~/.config/nvim
+ln -sf ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+
+# Install vim-plug for Vim
+if [ ! -f ~/.vim/autoload/plug.vim ]; then
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+# Install vim-plug for Neovim
+if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
+  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+echo "Dotfiles setup completed. Please restart your terminal."
